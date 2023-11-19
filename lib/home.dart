6651667dcea33a1ac23c5d54425e582d9fb4b0d2
alli komayha <Ali Komayha>
2023-11-20 00:15:39 +0200

    Creating female page and addind its navigating button

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'malePage.dart';
+import 'femalePage.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -20,20 +23,57 @@ class _HomeState extends State<Home> {
       body: Center(child: Column(
 
         children: [
-          const SizedBox(height: 25.0,),
-          
+          const SizedBox(height: 150.0,),
+
+          /// male page button
           ElevatedButton(
-              child: const Icon(Icons.man_2_rounded, size: 60),
+
+              child:  Container(
+                   // width: 120,
+                  // height: 120,
+
+                // decoration: BoxDecoration(
+                //   color: Colors.lightBlueAccent,
+                //   //borderRadius: BorderRadius.circular(10),
+                //   ),
+
+                child: Icon(Icons.man_2_rounded, size: 100, color: Colors.white,),
+              ),
+
               onPressed: (){
 
                 Navigator.of(context).push(
                   MaterialPageRoute(builder: (context)=> const malePage(),
                   )
-
                 );
-
               },
-          )
+          ),
+          const SizedBox(height: 10.0,),
+          const Text("Male ", style: TextStyle(fontSize: 25.0,color: Colors.blue)),
+          const SizedBox(height: 150.0,),
+
+
+        /// female page button
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => femalePage()),
+            );
+          },
+          child: Container(
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(Icons.woman_2_rounded, size: 100, color: Colors.white),
+          ),
+
+        ),
+          const SizedBox(height: 10.0,),
+          const Text("Female ", style: TextStyle(fontSize: 25.0,color: Colors.pink)),
         ],
 
       ),)
